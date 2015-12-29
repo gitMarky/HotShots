baa90c3327316c2c353b7931ddd4e7584ac52692
Mark <markh4b@hotmail.com>
2015-12-29 13:40:05 +0100

    Handling: Added confirm option for less waiting periods

--- HotShots.ocs/System.ocg/Mod_ClonkControl.c
+++ HotShots.ocs/System.ocg/Mod_ClonkControl.c
@@ -8,7 +8,16 @@
 public func ObjectControl(int plr, int ctrl, int x, int y, int strength, bool repeat, bool release)
 {
 	// no controls while crew selection is active
-	if (TurnSelectionCountdown()->IsActive()) return false;
+	if (TurnSelectionCountdown()->IsActive())
+	{
+		if(ctrl == CON_Interact && release)
+		{
+			TurnSelectionCountdown()->Abort();
+			return true;
+		}
+
+		return false;
+	}
 	
 	return _inherited(plr, ctrl, x, y, strength, repeat, release, ...);
 }
