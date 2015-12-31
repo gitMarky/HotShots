e18d6915f779c4220e098e494a9d4a569bd42754
Mark <markh4b@hotmail.com>
2015-12-31 13:36:18 +0100

    Basic island

--- HotShots.ocs/Map.c
+++ HotShots.ocs/Map.c
@@ -7,8 +7,8 @@ protected func InitializeMap(proplist map)
 	// Map size: all other settings depend on this value.
 	// The map size depends on the number of players.
 	var players = GetStartupPlayerCount();
-	var map_width = 60 + players * 20;
-	var map_height = BoundBy(3 * map_width / 4, 75, 150);
+	var map_width = 100 + players * 40;
+	var map_height = BoundBy(map_width / 3, 50, 150);
 
 	map->Resize(map_width, map_height);
 
@@ -16,10 +16,13 @@ protected func InitializeMap(proplist map)
 	var water = {Algo = MAPALGO_Rect, X = 0, Y = map_height * 8 / 10, Wdt = map_width, Hgt = map_height * 2 / 10};
 	Draw("Water", water);
 
-	// Draw a random surface
-	var surface = {Algo = MAPALGO_Rect, X = 0, Y = map.Hgt * 7 / 10, Wdt = map.Wdt, Hgt = (map.Hgt + 1) * 3 / 10};
-	surface = {Algo = MAPALGO_Turbulence, Seed = Random(65536), Op = surface, Amplitude = [25, 15], Scale = 10, Iterations = 3};
-	Draw("Earth", surface);	
+	// Draw the main surface: an island with the polygon algorithm.
+	var x_points = [              0, 15 * map_width / 100, 48 * map_width / 100, 52 * map_width / 100, 85 * map_width / 100, map_width];
+	var y_points = [map_height + 10, 70 * map_height /100, 60 * map_height /100, 60 * map_height /100, 70 * map_height /100, map_height + 10];
+	var island = {Algo = MAPALGO_Polygon, X = x_points, Y = y_points};
+	island = {Algo = MAPALGO_Turbulence, Seed = Random(65536), Op = island, Amplitude = [25, 15], Scale = 10, Iterations = 1};	
+	Draw("Earth", island);	
+
 
 	// Return true to tell the engine a map has been successfully created.
 	return true;
